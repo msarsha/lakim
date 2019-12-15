c95650f8da7618df81a0a455d9ef85de447ffe93
Matan SarSha <smatan84@gmail.com>
2019-12-15 21:14:23 +0200

    progress

--- src/app/shared/services/schedule.service.ts
+++ src/app/shared/services/schedule.service.ts
@@ -6,47 +6,60 @@ import {SettingsService} from './settings.service';
 import {switchMap} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ScheduleService {
 
-  constructor(private settingsService: SettingsService) {
-  }
-
-  getAvailableSlots(interval: number): Observable<any> {
-    return this.settingsService.settings$
-        .pipe(
-            switchMap((settings: Settings) => {
-              return this
-                  .buildWorkingHours(interval,
-                      settings.workingHours.from as HoursMinutesPair,
-                      settings.workingHours.to as HoursMinutesPair);
-            })
-        );
-
-    return of([]);
-  }
-
-  private buildWorkingHours(interval: number, from: HoursMinutesPair, to: HoursMinutesPair): HoursMinutesPair[] {
-    const workingHours = [];
-    const dateAtMidnight = new Date().setHours(0, 0, 0, 0);
-
-    const today = new Date(dateAtMidnight);
-
-    let currentDayAndTime = new Date(dateAtMidnight);
-
-    const thisDay = today.getDay();
-    let currentDay = today.getDay();
-
-    while (thisDay === currentDay) {
-      const hours = format(currentDayAndTime, 'HH');
-      const minutes = format(currentDayAndTime, 'mm');
-      workingHours.push({hours, minutes});
-
-      currentDayAndTime = addMinutes(currentDayAndTime, interval);
-      currentDay = currentDayAndTime.getDay();
+    constructor(private settingsService: SettingsService) {
     }
 
-    return workingHours;
-  }
+    getAvailableSlots(interval: number): Observable<any> {
+        return this.settingsService.settings$
+            .pipe(
+                switchMap((settings: Settings) => {
+                    return this
+                        .buildWorkingHours(interval,
+                            settings.workingHours.from as HoursMinutesPair,
+                            settings.workingHours.to as HoursMinutesPair);
+                })
+            );
+    }
+
+    private buildWorkingHours(interval: number, from: HoursMinutesPair, to: HoursMinutesPair): HoursMinutesPair[] {
+        const workingHours = [];
+        const dateAtMidnight = new Date().setHours(0, 0, 0, 0);
+
+        const today = new Date(dateAtMidnight);
+
+        let currentDayAndTime = new Date(dateAtMidnight);
+
+        const thisDay = today.getDay();
+        let nextDay = today.getDay();
+
+        while (thisDay === nextDay) {
+            const hours = format(currentDayAndTime, 'HH');
+            const minutes = format(currentDayAndTime, 'mm');
+
+            if (this.isBetween({hours: Number(hours), minutes: Number(minutes)}, from, to)) {
+                workingHours.push({hours, minutes});
+            }
+
+            currentDayAndTime = addMinutes(currentDayAndTime, interval);
+            nextDay = currentDayAndTime.getDay();
+        }
+
+        return workingHours;
+    }
+
+    private isBetween(param: HoursMinutesPair, from: HoursMinutesPair, to: HoursMinutesPair): boolean {
+        const fromMinutes = this.toMinutes(from);
+        const toMinutes = this.toMinutes(to);
+        const pairMinutes = this.toMinutes(param);
+
+        return (pairMinutes >= fromMinutes && pairMinutes <= toMinutes);
+    }
+
+    private toMinutes(pair: HoursMinutesPair): number {
+        return (pair.hours * 60) + pair.minutes;
+    }
 }
